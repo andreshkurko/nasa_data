e15ffe8a7fabf16a56372f660c2ac56aaff03954
Andre Shkurko <codingdevhub@gmail.com>
2020-06-16 22:27:45 +0300

    added logo NASA

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {Img} from 'react-image';
+
+const myComponent = () => <Img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/NASA_Wormball_logo.svg/512px-NASA_Wormball_logo.svg.png" />
 
 export default function Home() {
  
@@ -7,6 +10,7 @@ export default function Home() {
     <div className="home">
       <Link className="link" to="/nasaphoto" >See into the stars!</Link>
       <Link className="home">created by Andre Shkurko</Link>
+      <div><img className="nasa-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/NASA_Wormball_logo.svg/512px-NASA_Wormball_logo.svg.png" width="100px" height="100px"></img></div>
     </div>
   );
 }
